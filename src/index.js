d86a6107e0f3c474212242486cb5e460c808eeb3
Elghazy1 <yazidelghazy@gmail.com>
2021-10-17 23:28:26 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Client from "./Client";
+import ClientForm from "./ClientForm";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  state = {
+    clients: [
+      { id: 1, nom: "Lior Chamla" },
+      { id: 2, nom: "Magali Pernin" },
+      { id: 3, nom: "Joseph Durand" },
+    ],
+  };
+
+  handleDelete = (id) => {
+    const clients = [...this.state.clients];
+    const index = clients.findIndex((client) => client.id === id);
+
+    clients.splice(index, 1);
+
+    this.setState({ clients });
+  };
+
+  handleAdd = (client) => {
+    const clients = [...this.state.clients];
+    clients.push(client);
+
+    this.setState({ clients });
+  };
+
+  render() {
+    const title = "Liste des clients";
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <ul>
+          {this.state.clients.map((client) => (
+            <Client
+              key={client.id}
+              details={client}
+              onDelete={this.handleDelete}
+            />
+          ))}
+        </ul>
+        <ClientForm onClientAdd={this.handleAdd} />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
